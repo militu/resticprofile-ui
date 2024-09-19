3cf94cfd51898eb884a3878f006798be32e01db2
militu <mazzeo.victor@gmail.com>
2024-09-19 13:57:21 +0200

    standalone output

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "standalone",
+  output: "standalone",
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
